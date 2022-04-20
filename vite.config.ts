95a9233b6b9512bda0163d571c7dbf38a16970f0
Stefan Heim <stefan.heim@tip.co.at>
2022-04-20 11:00:16 +0200

    Change vite to host 0.0.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   },
+  server: {
+    host: "0.0.0.0"
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
